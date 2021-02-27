07cf91fc354d974dfe259198050ad6cd7e6fa00d
Krishna Jhapate <68365551+krishnajhapate@users.noreply.github.com>
2021-02-27 22:55:44 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBtAE6wWoz7bj-jSkgFJiB2gu9OJlBr1fA",
-  authDomain: "challenge-695d5.firebaseapp.com",
-  projectId: "challenge-695d5",
-  storageBucket: "challenge-695d5.appspot.com",
-  messagingSenderId: "846316400517",
-  appId: "1:846316400517:web:8da3c790eae20e3aabdeec",
-  measurementId: "G-VXDQE544LW",
+//   Paste your credentials here from firebase
 });
 
 const db = firebaseApp.firestore();
